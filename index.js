5307da94c64cd33b3a89177bf5bf63f70c2aeb5e
absentees <scottblissett@gmail.com>
2017-06-20 18:20:38 +1000

    poop

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 #!/usr/bin/env node
 'use strict';
+
 require('babel-polyfill');
+var Xray = require('x-ray');
+var x = Xray();
 
 (() => {
-	console.log("hi");
+
+	x('https://www.allin.wtf/current-issue', {
+		title: 'p',
+		url: x('p', ['a@href'])
+	})(function(err, obj){
+		console.log(obj);
+	});
+
 })();
+
+// x('http://www.imdb.com/', {
+//   title: ['title'],
+//   links: x('.rhs-body .rhs-row', [{
+//     text: 'a',
+//     href: 'a@href',
+//     next_page: x('a@href', {
+//       title: 'title',
+//       heading: 'h1'
+//     })
+//   }])
+// })(function (err, obj) {
+//   console.log(err, obj)
+// })
